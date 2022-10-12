6fe03d617fed1d9e01ae03cbcb4a6f0025a9dab6
Carlos Eduardo <eduardo10279@gmail.com>
2022-10-12 14:22:44 -0300

    Criação das Lista e Filas.

--- src/main/java/br/com/trabPro2/model/Faculdade.java
+++ src/main/java/br/com/trabPro2/model/Faculdade.java
@@ -1,5 +1,54 @@
 package br.com.trabPro2.model;
 
-public class Faculdade implements PessoaJuridica {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Faculdade implements PessoaJuridica {
+    private Diretor diretor;
+    private List<Curso> cursos = new ArrayList<>();
+    private List<Coordenador> coordenadors = new ArrayList<>();
+    private List<SecAcademica> secAcademicas = new ArrayList<>();
+    private List<SecFinanceira> secFinanceiras = new ArrayList<>();
+
+    public List<Coordenador> getCoordenadors() {
+        return coordenadors;
+    }
+
+    public void setCoordenadors(List<Coordenador> coordenadors) {
+        this.coordenadors = coordenadors;
+    }
+
+    public List<SecAcademica> getSecAcademicas() {
+        return secAcademicas;
+    }
+
+    public void setSecAcademicas(List<SecAcademica> secAcademicas) {
+        this.secAcademicas = secAcademicas;
+    }
+
+    public List<SecFinanceira> getSecFinanceiras() {
+        return secFinanceiras;
+    }
+
+    public void setSecFinanceiras(List<SecFinanceira> secFinanceiras) {
+        this.secFinanceiras = secFinanceiras;
+    }
+
+    public Diretor getDiretor() {
+        return diretor;
+    }
+
+    public void setDiretor(Diretor diretor) {
+        this.diretor = diretor;
+    }
+
+    public List<Curso> getCursos() {
+
+        return cursos;
+    }
+
+    public void setCursos(List<Curso> cursos) {
+
+        this.cursos = cursos;
+    }
 }
--- src/main/java/br/com/trabPro2/model/SecFinanceira.java
+++ src/main/java/br/com/trabPro2/model/SecFinanceira.java
@@ -1,5 +1,15 @@
 package br.com.trabPro2.model;
 
-public class SecFinanceira extends Funcionario implements PessoaFisica,Secretaria {
+import br.com.trabPro2.util.Fila;
 
+public class SecFinanceira extends Funcionario implements PessoaFisica,Secretaria {
+    private Fila<PessoaFisica> pessoaFisicaFila = new Fila<>();
+
+    public Fila<PessoaFisica> getPessoaFisicaFila() {
+        return pessoaFisicaFila;
+    }
+
+    public void setPessoaFisicaFila(Fila<PessoaFisica> pessoaFisicaFila) {
+        this.pessoaFisicaFila = pessoaFisicaFila;
+    }
 }
