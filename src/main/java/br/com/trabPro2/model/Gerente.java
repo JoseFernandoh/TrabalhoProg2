3bc9f0d7f61ca8fa99d1894e8502a067c550c9aa
eduardo <eduardo10279@gmail.com>
2022-10-12 20:21:26 -0300

    Metodos addFila

--- src/main/java/br/com/trabPro2/model/Gerente.java
+++ src/main/java/br/com/trabPro2/model/Gerente.java
@@ -1,6 +1,7 @@
 package br.com.trabPro2.model;
 
 import br.com.trabPro2.util.Fila;
+import br.com.trabPro2.util.FilaVaziaException;
 
 public class Gerente extends Funcionario implements PessoaFisica {
     private Fila<Pessoa> pessoaFila = new Fila<>();
@@ -11,5 +12,16 @@ public class Gerente extends Funcionario implements PessoaFisica {
 
     public void setPessoaFila(Fila<Pessoa> pessoaFila) {
         this.pessoaFila = pessoaFila;
+    }
+
+    public void addFilaPessoa(Pessoa pessoa){
+        this.pessoaFila.add(pessoa);
+    }
+    public void addFilaAllPessoa(Fila<Pessoa> pessoaFila){
+        this.pessoaFila.addAll(pessoaFila);
+    }
+    public void atenderFilaPessoa() throws FilaVaziaException {
+        Pessoa pessoa = this.pessoaFila.pegarRemove();
+        System.out.println("Atendendo Pessoa");
     }
 }
--- src/main/java/br/com/trabPro2/model/SecFinanceira.java
+++ src/main/java/br/com/trabPro2/model/SecFinanceira.java
@@ -1,6 +1,7 @@
 package br.com.trabPro2.model;
 
 import br.com.trabPro2.util.Fila;
+import br.com.trabPro2.util.FilaVaziaException;
 
 public class SecFinanceira extends Funcionario implements PessoaFisica,Secretaria {
     private Fila<PessoaFisica> pessoaFisicaFila = new Fila<>();
@@ -12,4 +13,16 @@ public class SecFinanceira extends Funcionario implements PessoaFisica,Secretari
     public void setPessoaFisicaFila(Fila<PessoaFisica> pessoaFisicaFila) {
         this.pessoaFisicaFila = pessoaFisicaFila;
     }
+    public void addFilaPessoaFisica(PessoaFisica pessoaFisica){
+        this.pessoaFisicaFila.add(pessoaFisica);
+    }
+    public void addFilaAllPessoaFisica(Fila<PessoaFisica> pessoaFisicaFila){
+        this.pessoaFisicaFila.addAll(pessoaFisicaFila);
+    }
+    public void atenderFilaPessoaFisica() throws FilaVaziaException {
+        PessoaFisica pessoaFisica = this.pessoaFisicaFila.pegarRemove();
+        System.out.println("Atendendo Pessoa Fisica");
+
+    }
 }
+
