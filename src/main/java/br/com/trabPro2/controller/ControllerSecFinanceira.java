c01c0367216bbfc98d88e894b0ad6c9129b22d57
eduardo <eduardo10279@gmail.com>
2022-10-13 20:46:01 -0300

    Criação dos controll

--- src/main/java/br/com/trabPro2/controller/ControllerSecFinanceira.java
+++ src/main/java/br/com/trabPro2/controller/ControllerSecFinanceira.java
@@ -1,4 +1,23 @@
 package br.com.trabPro2.controller;
 
+import br.com.trabPro2.model.SecFinanceira;
+
+import java.util.Scanner;
+
 public class ControllerSecFinanceira {
+    public SecFinanceira criarSecFinanceira(){
+        Scanner scan = new Scanner(System.in);
+        SecFinanceira secFinanceira = new SecFinanceira();
+        System.out.println("Digite seu ID: ");
+        secFinanceira.setId(scan.nextInt());
+
+        System.out.println("Digite seu salario: ");
+        secFinanceira.setSalario(scan.nextFloat());
+
+        System.out.println("Digite seu nome:  ");
+        secFinanceira.setNome(scan.nextLine());
+
+        return secFinanceira;
+
+    }
 }
