03749e453edf22d375e78d0a374bab5400d571f4
victor <victorfortes34@gmail.com>
2022-10-13 20:51:17 -0300

    correção Scanner

--- src/main/java/br/com/trabPro2/controller/ControllerAgencia.java
+++ src/main/java/br/com/trabPro2/controller/ControllerAgencia.java
@@ -1,4 +1,18 @@
 package br.com.trabPro2.controller;
 
+import br.com.trabPro2.model.Agencia;
+
+import java.util.Scanner;
+
 public class ControllerAgencia {
+    public Agencia CriarAgencia(){
+
+        Scanner scan = new Scanner(System.in);
+        Agencia agencia = new Agencia();
+
+        System.out.println("Informe sua ID:");
+       agencia.setId(scan.nextLong());
+
+return agencia;
+    }
 }
--- src/main/java/br/com/trabPro2/controller/ControllerBanco.java
+++ src/main/java/br/com/trabPro2/controller/ControllerBanco.java
@@ -1,4 +1,20 @@
 package br.com.trabPro2.controller;
 
+import br.com.trabPro2.model.Banco;
+
+import java.util.Scanner;
+
 public class ControllerBanco {
+
+    public Banco criarBanco(){
+
+        Scanner scan = new Scanner(System.in);
+        Banco banco = new Banco();
+
+        System.out.println("Informe a ID do Banco:");
+        banco.setId(scan.nextLong());
+
+        return banco;
+
+    }
 }
