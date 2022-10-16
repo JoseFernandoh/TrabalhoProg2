1db88747ea96ba4bc1763a610c7f2738e0581439
Jose Fernando <josecernandl1565@gmail.com>
2022-10-16 09:54:04 -0300

    Adicionando Hibernate

--- src/main/java/br/com/trabPro2/controller/ControllerAluno.java
+++ src/main/java/br/com/trabPro2/controller/ControllerAluno.java
@@ -6,22 +6,4 @@ import java.util.Scanner;
 
 public class ControllerAluno {
 
-
-    public Aluno criarAluno(){
-
-        Scanner scan = new Scanner(System.in);
-           Aluno aluno = new Aluno ();
-
-        System.out.println("Digite sua matricula:");
-         aluno.setMatricula(scan.nextInt());
-
-        System.out.println("Informe sua ID");
-        aluno.setId(scan.nextLong());
-
-        System.out.println("Digite seu nome:");
-        aluno.setNome(scan.nextLine());
-
-return aluno ;
-
-    }
 }
