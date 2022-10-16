01d4757c62c27c0c34e9cb1c23c76e5b7e0373f0
Jose Fernando <josecernandl1565@gmail.com>
2022-10-16 16:54:26 -0300

    Revert "Adicionando Hibernate"
    This reverts commit 1db88747ea96ba4bc1763a610c7f2738e0581439.

--- src/main/java/br/com/trabPro2/controller/ControllerContaCorrente.java
+++ src/main/java/br/com/trabPro2/controller/ControllerContaCorrente.java
@@ -6,4 +6,17 @@ import java.util.Scanner;
 
 public class ControllerContaCorrente {
 
+    public ContaCorrente criarContaCorrente(){
+
+        Scanner scan = new Scanner(System.in);
+        ContaCorrente contacorrente = new ContaCorrente();
+        System.out.println("Digite a Conta da Conta Corrente:");
+contacorrente.setConta(scan.nextInt());
+        System.out.println("Digite o Saldo da Conta Corrente:");
+contacorrente.setSaldo(scan.nextFloat());
+        System.out.println("Informe a Agencia do Conta Corrente");
+        contacorrente.setAgencia(scan.nextInt());
+
+return contacorrente;
+    }
 }
