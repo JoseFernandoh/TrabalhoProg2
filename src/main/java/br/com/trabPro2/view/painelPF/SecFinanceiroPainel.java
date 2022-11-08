e01574d8d3d8bafd98ed9669ff8c5671e2555ada
eduardo <eduardo10279@gmail.com>
2022-11-08 10:21:04 -0300

    Criação Telas dos Secretarios

--- src/main/java/br/com/trabPro2/view/painelPF/SecFinanceiroPainel.java
+++ src/main/java/br/com/trabPro2/view/painelPF/SecFinanceiroPainel.java
@@ -1,10 +1,145 @@
 package br.com.trabPro2.view.painelPF;
 
+import br.com.trabPro2.db.ControllerDB;
+import br.com.trabPro2.model.SecFinanceira;
+import br.com.trabPro2.util.ControllerPainel;
+import br.com.trabPro2.view.TelaPrincipal;
+
 import javax.swing.*;
+import javax.swing.text.MaskFormatter;
+import java.awt.*;
+import java.text.SimpleDateFormat;
 
 public class SecFinanceiroPainel extends JPanel {
 
-    // Gloria
-    // Exemplo Professor Painel
+    private final JFrame jframe;
+
+    public SecFinanceiroPainel(JFrame jFrame) {
+
+        this.jframe = jFrame;
+
+        setLayout(new BorderLayout());
+
+        JTabbedPane tab = new JTabbedPane();
+
+        tab.addTab("Cadastro", painelCadastro());
+
+        this.add(tab);
+
+    }
+
+    private JPanel painelCadastro(){
+        JPanel painel = new JPanel(new BorderLayout());
+        GridBagConstraints gridBagConstraints = new GridBagConstraints();
+        JLabel label;
+        JTextField input;
+        JButton butao;
+
+        gridBagConstraints.insets = new Insets(5,0,5,0);
+
+        label = new JLabel("Cadastrar Sec.Financeiro");
+        label.setPreferredSize(new Dimension(450, 30));
+        label.setFont(new Font("Arial", Font.PLAIN, 25));
+        label.setHorizontalTextPosition(SwingConstants.CENTER);
+        painel.add(label, BorderLayout.PAGE_START);
+
+        JPanel form = new JPanel(new GridBagLayout());
+
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 0;
+        gridBagConstraints.gridwidth = 2;
+
+        label = new JLabel("Nome: ");
+        label.setFont(new Font("Arial", Font.PLAIN, 15));
+        label.setPreferredSize(new Dimension(150,20));
+        form.add(label, gridBagConstraints);
+
+        gridBagConstraints.gridy++;
+
+        input = new JTextField();
+        input.setPreferredSize(new Dimension(150,20));
+        input.setName("Nome");
+        form.add(input, gridBagConstraints);
+
+        gridBagConstraints.gridy++;
+
+        label = new JLabel("Data de Nascimento:");
+        label.setFont(new Font("Arial", Font.PLAIN, 15));
+        label.setPreferredSize(new Dimension(150,20));
+        form.add(label, gridBagConstraints);
+
+        gridBagConstraints.gridy++;
+
+        try{
+            input = new JFormattedTextField(new MaskFormatter("##/##/####"));
+            input.setPreferredSize(new Dimension(150,20));
+            input.setName("DataNascimento");
+            form.add(input, gridBagConstraints);
+        }catch (Exception e){
+            JOptionPane.showMessageDialog(null, "Data Invalida");
+            e.printStackTrace();
+        }
+
+
+        gridBagConstraints.gridy++;
+
+        label = new JLabel("CPF:");
+        label.setFont(new Font("Arial", Font.PLAIN, 15));
+        label.setPreferredSize(new Dimension(150,20));
+        form.add(label, gridBagConstraints);
+
+        gridBagConstraints.gridy++;
+
+        try{
+            input = new JFormattedTextField(new MaskFormatter("###.###.###-##"));
+            input.setPreferredSize(new Dimension(150,20));
+            input.setName("CPF");
+            form.add(input, gridBagConstraints);
+        }catch (Exception e){
+            JOptionPane.showMessageDialog(null, "CPF Invalido");
+            e.printStackTrace();
+        }
+
+
+        painel.add(form, BorderLayout.CENTER);
+
+        JPanel pageEnd = new JPanel(new FlowLayout());
+
+        butao = new JButton("Voltar");
+        butao.setPreferredSize(new Dimension(150,20));
+        pageEnd.add(butao);
+
+        butao.addActionListener(e-> {
+            jframe.dispose();
+            TelaPrincipal telaPrincipal = new TelaPrincipal();
+            telaPrincipal.inicializarPrograma();
+        });
+
+        butao = new JButton("Cadastro");
+        butao.setPreferredSize(new Dimension(150,20));
+        pageEnd.add(butao);
+
+        butao.addActionListener(e -> {
+            try {
+                SecFinanceira secFinanceira = new SecFinanceira();
+                secFinanceira.setNome(ControllerPainel.getValuePainelName(form,"Nome"));
+                secFinanceira.setCpf(ControllerPainel.getValuePainelName(form,"CPF"));
+                secFinanceira.setDataNascimento(new SimpleDateFormat("dd/MM/yyyy").parse(ControllerPainel.getValuePainelName(form,"DataNascimento")));
+
+                ControllerDB.addDB(secFinanceira);
+
+            } catch (Exception ex) {
+                JOptionPane.showMessageDialog(null, "Não foi Possivel Cadastrar Tente Novamente");
+                ex.printStackTrace();
+            }
+
+            ControllerPainel.clearForm(form);
+
+        });
+
+        painel.add(pageEnd,BorderLayout.PAGE_END);
+
+        return painel;
+    }
 
 }
