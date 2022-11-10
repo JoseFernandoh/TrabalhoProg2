d8bfeb0432a04c8a54cfb08bf4dd1307ffb24b79
Mario-78 <mariojsws1@gmail.com>
2022-11-10 07:16:10 -0300

    Novas funções de inserção de dados

--- src/main/java/br/com/trabPro2/view/painelPF/CoordernadorPainel.java
+++ src/main/java/br/com/trabPro2/view/painelPF/CoordernadorPainel.java
@@ -1,6 +1,12 @@
 package br.com.trabPro2.view.painelPF;
 
+import br.com.trabPro2.model.Gerente;
+import br.com.trabPro2.model.Professor;
+import br.com.trabPro2.util.ControllerPainel;
+import br.com.trabPro2.view.TelaPrincipal;
+
 import javax.swing.*;
+import javax.swing.text.MaskFormatter;
 import java.awt.*;
 import java.text.SimpleDateFormat;
 
@@ -14,6 +20,7 @@ public class CoordernadorPainel extends JPanel {
         JTabbedPane tab = new JTabbedPane();
 
         tab.addTab("Cadastro", painelCadastro());
+        tab.addTab("Cadastrar Conta", painelCriarContraAddCoordenador());
 
         this.add(tab);
 
@@ -49,6 +56,7 @@ public class CoordernadorPainel extends JPanel {
 
         input = new JTextField();
         input.setPreferredSize(new Dimension(150,20));
+        input.setName("Nome");
         form.add(input, gridBagConstraints);
 
         gridBagConstraints.gridy++;
@@ -60,9 +68,15 @@ public class CoordernadorPainel extends JPanel {
 
         gridBagConstraints.gridy++;
 
-        input = new JTextField();
-        input.setPreferredSize(new Dimension(150,20));
-        form.add(input, gridBagConstraints);
+        try{
+            input = new JFormattedTextField(new MaskFormatter("##/##/####"));
+            input.setPreferredSize(new Dimension(150,20));
+            input.setName("DataNascimento");
+            form.add(input, gridBagConstraints);
+        }catch (Exception e){
+            JOptionPane.showMessageDialog(null, "Data Invalida");
+            e.printStackTrace();
+        }
 
         gridBagConstraints.gridy++;
 
@@ -73,9 +87,15 @@ public class CoordernadorPainel extends JPanel {
 
         gridBagConstraints.gridy++;
 
-        input = new JTextField();
-        input.setPreferredSize(new Dimension(150,20));
-        form.add(input, gridBagConstraints);
+        try{
+            input = new JFormattedTextField(new MaskFormatter("###.###.###-##"));
+            input.setPreferredSize(new Dimension(150,20));
+            input.setName("CPF");
+            form.add(input, gridBagConstraints);
+        }catch (Exception e){
+            JOptionPane.showMessageDialog(null, "CPF Invalido");
+            e.printStackTrace();
+        }
 
         painel.add(form, BorderLayout.CENTER);
 
@@ -108,6 +128,130 @@ public class CoordernadorPainel extends JPanel {
         });
 
         painel.add(pageEnd,BorderLayout.PAGE_END);
+
+        return painel;
+    }
+
+    private JPanel painelCriarContraAddCoordenador(){
+        JPanel painel = new JPanel(new BorderLayout());
+        GridBagConstraints gridBagConstraints = new GridBagConstraints();
+        JLabel label;
+        JButton butao;
+        JTextField input;
+
+        gridBagConstraints.insets = new Insets(5,0,5,0);
+
+        label = new JLabel("Cadastrar Conta Coordenador");
+        label.setPreferredSize(new Dimension(450, 30));
+        label.setFont(new Font("Arial", Font.PLAIN, 25));
+        label.setHorizontalTextPosition(SwingConstants.CENTER);
+        painel.add(label, BorderLayout.PAGE_START);
+
+        JPanel form = new JPanel(new GridBagLayout());
+
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 0;
+        gridBagConstraints.gridwidth = 6;
+
+        JPanel labelCoordenador = new JPanel(new FlowLayout());
+
+        label = new JLabel("Coordenador: ");
+        label.setFont(new Font("Arial", Font.PLAIN, 15));
+        label.setPreferredSize(new Dimension(100,20));
+        labelCoordenador.add(label);
+
+        label = new JLabel("Nenhuma Pessoa Selecionada");
+        label.setFont(new Font("Arial", Font.PLAIN, 15));
+        label.setPreferredSize(new Dimension(200,20));
+        labelCoordenador.add(label);
+
+        butao = new JButton("Procurar");
+        butao.setPreferredSize(new Dimension(100,20));
+        labelCoordenador.add(butao);
+
+        butao.addActionListener(e->{
+            ControllerPainel.TelaProcurarBean(Coordenador.class);
+        });
+
+        form.add(labelCoordenador,gridBagConstraints);
+
+        gridBagConstraints.gridy++;
+
+        JPanel labelGerente = new JPanel(new FlowLayout());
+
+        label = new JLabel("Gerente: ");
+        label.setFont(new Font("Arial", Font.PLAIN, 15));
+        label.setPreferredSize(new Dimension(100,20));
+        labelGerente.add(label);
+
+        label = new JLabel("Nenhuma Pessoa Selecionada");
+        label.setFont(new Font("Arial", Font.PLAIN, 15));
+        label.setPreferredSize(new Dimension(200,20));
+        labelGerente.add(label);
+
+        butao = new JButton("Procurar");
+        butao.setPreferredSize(new Dimension(100,20));
+        labelGerente.add(butao);
+
+        butao.addActionListener(e->{
+            ControllerPainel.TelaProcurarBean(Gerente.class);
+        });
+
+        form.add(labelGerente,gridBagConstraints);
+
+        gridBagConstraints.gridy++;
+
+        label = new JLabel("Numero da Conta: ");
+        label.setFont(new Font("Arial", Font.PLAIN, 15));
+        label.setPreferredSize(new Dimension(150,20));
+        form.add(label, gridBagConstraints);
+
+        gridBagConstraints.gridy++;
+
+        input = new JTextField();
+        input.setPreferredSize(new Dimension(150,20));
+        input.setName("Conta");
+        form.add(input, gridBagConstraints);
+
+        gridBagConstraints.gridy++;
+
+        label = new JLabel("Numero da Agencia: ");
+        label.setFont(new Font("Arial", Font.PLAIN, 15));
+        label.setPreferredSize(new Dimension(150,20));
+        form.add(label, gridBagConstraints);
+
+        gridBagConstraints.gridy++;
+
+        input = new JTextField();
+        input.setPreferredSize(new Dimension(150,20));
+        input.setName("Agencia");
+        form.add(input, gridBagConstraints);
+
+        painel.add(form,BorderLayout.CENTER);
+
+        JPanel pageEnd = new JPanel(new FlowLayout());
+
+        butao = new JButton("Voltar");
+        butao.setPreferredSize(new Dimension(150,20));
+        pageEnd.add(butao);
+
+        butao.addActionListener(e-> {
+            jframe.dispose();
+            TelaPrincipal telaPrincipal = new TelaPrincipal();
+            telaPrincipal.inicializarPrograma();
+        });
+
+        butao = new JButton("Cadastro");
+        butao.setPreferredSize(new Dimension(150,20));
+        pageEnd.add(butao);
+
+        butao.addActionListener(e -> {
+
+        });
+
+        painel.add(pageEnd,BorderLayout.PAGE_END);
+
+        System.out.println("Dentro");
 
         return painel;
     }
--- src/main/java/br/com/trabPro2/view/painelPF/DiretorPainel.java
+++ src/main/java/br/com/trabPro2/view/painelPF/DiretorPainel.java
@@ -1,6 +1,11 @@
 package br.com.trabPro2.view.painelPF;
 
+import br.com.trabPro2.model.Gerente;
+import br.com.trabPro2.util.ControllerPainel;
+import br.com.trabPro2.view.TelaPrincipal;
+
 import javax.swing.*;
+import javax.swing.text.MaskFormatter;
 import java.awt.*;
 import java.text.SimpleDateFormat;
 
@@ -14,6 +19,7 @@ public class DiretorPainel extends JPanel {
         JTabbedPane tab = new JTabbedPane();
 
         tab.addTab("Cadastro", painelCadastro());
+        tab.addTab("Cadastrar Conta", painelCriarContraAddDiretor());
 
         this.add(tab);
 
@@ -49,6 +55,7 @@ public class DiretorPainel extends JPanel {
 
         input = new JTextField();
         input.setPreferredSize(new Dimension(150,20));
+        input.setName("Nome");
         form.add(input, gridBagConstraints);
 
         gridBagConstraints.gridy++;
@@ -60,9 +67,15 @@ public class DiretorPainel extends JPanel {
 
         gridBagConstraints.gridy++;
 
-        input = new JTextField();
-        input.setPreferredSize(new Dimension(150,20));
-        form.add(input, gridBagConstraints);
+        try{
+            input = new JFormattedTextField(new MaskFormatter("##/##/####"));
+            input.setPreferredSize(new Dimension(150,20));
+            input.setName("DataNascimento");
+            form.add(input, gridBagConstraints);
+        }catch (Exception e){
+            JOptionPane.showMessageDialog(null, "Data Invalida");
+            e.printStackTrace();
+        }
 
         gridBagConstraints.gridy++;
 
@@ -73,9 +86,15 @@ public class DiretorPainel extends JPanel {
 
         gridBagConstraints.gridy++;
 
-        input = new JTextField();
-        input.setPreferredSize(new Dimension(150,20));
-        form.add(input, gridBagConstraints);
+        try{
+            input = new JFormattedTextField(new MaskFormatter("###.###.###-##"));
+            input.setPreferredSize(new Dimension(150,20));
+            input.setName("CPF");
+            form.add(input, gridBagConstraints);
+        }catch (Exception e){
+            JOptionPane.showMessageDialog(null, "CPF Invalido");
+            e.printStackTrace();
+        }
 
         painel.add(form, BorderLayout.CENTER);
 
@@ -107,6 +126,130 @@ public class DiretorPainel extends JPanel {
         });
 
         painel.add(pageEnd,BorderLayout.PAGE_END);
+
+        return painel;
+    }
+
+    private JPanel painelCriarContraAddDiretor(){
+        JPanel painel = new JPanel(new BorderLayout());
+        GridBagConstraints gridBagConstraints = new GridBagConstraints();
+        JLabel label;
+        JButton butao;
+        JTextField input;
+
+        gridBagConstraints.insets = new Insets(5,0,5,0);
+
+        label = new JLabel("Cadastrar Conta Diretor");
+        label.setPreferredSize(new Dimension(450, 30));
+        label.setFont(new Font("Arial", Font.PLAIN, 25));
+        label.setHorizontalTextPosition(SwingConstants.CENTER);
+        painel.add(label, BorderLayout.PAGE_START);
+
+        JPanel form = new JPanel(new GridBagLayout());
+
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 0;
+        gridBagConstraints.gridwidth = 6;
+
+        JPanel labelDiretor = new JPanel(new FlowLayout());
+
+        label = new JLabel("Diretor: ");
+        label.setFont(new Font("Arial", Font.PLAIN, 15));
+        label.setPreferredSize(new Dimension(100,20));
+        labelDiretor.add(label);
+
+        label = new JLabel("Nenhuma Pessoa Selecionada");
+        label.setFont(new Font("Arial", Font.PLAIN, 15));
+        label.setPreferredSize(new Dimension(200,20));
+        labelDiretor.add(label);
+
+        butao = new JButton("Procurar");
+        butao.setPreferredSize(new Dimension(100,20));
+        labelDiretor.add(butao);
+
+        butao.addActionListener(e->{
+            ControllerPainel.TelaProcurarBean(Diretor.class);
+        });
+
+        form.add(labelDiretor,gridBagConstraints);
+
+        gridBagConstraints.gridy++;
+
+        JPanel labelGerente = new JPanel(new FlowLayout());
+
+        label = new JLabel("Gerente: ");
+        label.setFont(new Font("Arial", Font.PLAIN, 15));
+        label.setPreferredSize(new Dimension(100,20));
+        labelGerente.add(label);
+
+        label = new JLabel("Nenhuma Pessoa Selecionada");
+        label.setFont(new Font("Arial", Font.PLAIN, 15));
+        label.setPreferredSize(new Dimension(200,20));
+        labelGerente.add(label);
+
+        butao = new JButton("Procurar");
+        butao.setPreferredSize(new Dimension(100,20));
+        labelGerente.add(butao);
+
+        butao.addActionListener(e->{
+            ControllerPainel.TelaProcurarBean(Gerente.class);
+        });
+
+        form.add(labelGerente,gridBagConstraints);
+
+        gridBagConstraints.gridy++;
+
+        label = new JLabel("Numero da Conta: ");
+        label.setFont(new Font("Arial", Font.PLAIN, 15));
+        label.setPreferredSize(new Dimension(150,20));
+        form.add(label, gridBagConstraints);
+
+        gridBagConstraints.gridy++;
+
+        input = new JTextField();
+        input.setPreferredSize(new Dimension(150,20));
+        input.setName("Conta");
+        form.add(input, gridBagConstraints);
+
+        gridBagConstraints.gridy++;
+
+        label = new JLabel("Numero da Agencia: ");
+        label.setFont(new Font("Arial", Font.PLAIN, 15));
+        label.setPreferredSize(new Dimension(150,20));
+        form.add(label, gridBagConstraints);
+
+        gridBagConstraints.gridy++;
+
+        input = new JTextField();
+        input.setPreferredSize(new Dimension(150,20));
+        input.setName("Agencia");
+        form.add(input, gridBagConstraints);
+
+        painel.add(form,BorderLayout.CENTER);
+
+        JPanel pageEnd = new JPanel(new FlowLayout());
+
+        butao = new JButton("Voltar");
+        butao.setPreferredSize(new Dimension(150,20));
+        pageEnd.add(butao);
+
+        butao.addActionListener(e-> {
+            jframe.dispose();
+            TelaPrincipal telaPrincipal = new TelaPrincipal();
+            telaPrincipal.inicializarPrograma();
+        });
+
+        butao = new JButton("Cadastro");
+        butao.setPreferredSize(new Dimension(150,20));
+        pageEnd.add(butao);
+
+        butao.addActionListener(e -> {
+
+        });
+
+        painel.add(pageEnd,BorderLayout.PAGE_END);
+
+        System.out.println("Dentro");
 
         return painel;
     }
